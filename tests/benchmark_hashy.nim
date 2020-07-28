3a348819c4e68c988b49e0631e0c398842cf3ce6
Ryan Oldenburg <guzba8@gmail.com>
2020-07-27 22:24:00 -0500

    hashing experimentation

---
+++ tests/benchmark_hashy.nim
@@ -0,0 +1,88 @@
+import random, fidget/opengl/perf
+
+include flatty/hashy
+
+var
+  r = initRand(1988)
+  short = newString(512)
+  long = newString(32 * 1024 * 1024)
+
+for i in 0 ..< short.len:
+  short[i] = r.rand(255).char
+
+for i in 0 ..< long.len:
+  long[i] = r.rand(255).char
+
+timeIt "short sdbm":
+  for i in 0 ..< 1_000_000:
+    let h = sdbm(short)
+    assert h != 0
+
+timeIt "short sdbm_fast":
+  for i in 0 ..< 1_000_000:
+    let h = sdbm_fast(short[0].unsafeAddr, short.len)
+    assert h != 0
+
+timeIt "short djb2":
+  for i in 0 ..< 1_000_000:
+    let h = djb2(short)
+    assert h != 0
+
+timeIt "short djb2_fast":
+  for i in 0 ..< 1_000_000:
+    let h = djb2_fast(short[0].unsafeAddr, short.len)
+    assert h != 0
+
+timeIt "short string":
+  for i in 0 ..< 1_000_000:
+    let h = hash(short)
+    assert h != 0
+
+timeIt "short nim byte hash":
+  for i in 0 ..< 1_000_000:
+    var h: Hash
+    for c in short:
+      h = h !& hash(c)
+    assert h != 0
+
+timeIt "short nim fast hash":
+  for i in 0 ..< 1_000_000:
+    var h = nim_fast(short[0].unsafeAddr, short.len)
+    assert h != 0
+
+timeIt "long sdbm":
+  for i in 0 ..< 100:
+    let h = sdbm(long)
+    assert h != 0
+
+timeIt "long sdbm_fast":
+  for i in 0 ..< 100:
+    let h = sdbm_fast(long[0].unsafeAddr, long.len)
+    assert h != 0
+
+timeIt "long djb2":
+  for i in 0 ..< 100:
+    let h = djb2(long)
+    assert h != 0
+
+timeIt "long djb2_fast":
+  for i in 0 ..< 100:
+    let h = djb2_fast(long[0].unsafeAddr, long.len)
+    assert h != 0
+
+timeIt "long string":
+  for i in 0 ..< 100:
+    let h = hash(long)
+    assert h != 0
+
+timeIt "long nim byte hash":
+  for i in 0 ..< 100:
+    var h: Hash
+    for c in long:
+      h = h !& hash(c)
+    assert h != 0
+
+timeIt "long nim fast hash":
+  for i in 0 ..< 100:
+    var h = nim_fast(long[0].unsafeAddr, long.len)
+    assert h != 0
---
+++ tests/hash_quality.nim
@@ -0,0 +1,73 @@
+include flatty/hashy
+
+const bucketCount = 32
+
+var buckets = newSeq[int](bucketCount)
+
+for i in 0 ..< 1_000_000:
+  var n = i
+  let
+    p = cast[ptr uint8](n.addr)
+    h = nim_fast(p, 8).uint64
+    b = (h mod buckets.len.uint64).int
+  buckets[b] = buckets[b] + 1
+
+echo "nim_fast"
+echo $buckets
+echo "---"
+
+buckets = newSeq[int](bucketCount)
+
+for i in 0 ..< 1_000_000:
+  var n = i
+  let
+    p = cast[ptr uint8](n.addr)
+    h = sdbm(p, 8).uint64
+    b = (h mod buckets.len.uint64).int
+  buckets[b] = buckets[b] + 1
+
+echo "sdbm"
+echo $buckets
+echo "---"
+
+buckets = newSeq[int](bucketCount)
+
+for i in 0 ..< 1_000_000:
+  var n = i
+  let
+    p = cast[ptr uint8](n.addr)
+    h = djb2(p, 8).uint64
+    b = (h mod buckets.len.uint64).int
+  buckets[b] = buckets[b] + 1
+
+echo "djb2"
+echo $buckets
+echo "---"
+
+buckets = newSeq[int](bucketCount)
+
+for i in 0 ..< 1_000_000:
+  var n = i
+  let
+    p = cast[ptr uint8](n.addr)
+    h = sdbm_fast(p, 8).uint64
+    b = (h mod buckets.len.uint64).int
+  buckets[b] = buckets[b] + 1
+
+echo "sdbm_fast"
+echo $buckets
+echo "---"
+
+buckets = newSeq[int](bucketCount)
+
+for i in 0 ..< 1_000_000:
+  var n = i
+  let
+    p = cast[ptr uint8](n.addr)
+    h = djb2_fast(p, 8).uint64
+    b = (h mod buckets.len.uint64).int
+  buckets[b] = buckets[b] + 1
+
+echo "djb2_fast"
+echo $buckets
+echo "---"
