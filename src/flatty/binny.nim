02785431cd725465cfd386c4bc8969f26fc18b50
Ryan Oldenburg <guzba8@gmail.com>
2020-10-18 18:20:38 -0500

    inline all read/write/add for inc perf

--- src/flatty/binny.nim
+++ src/flatty/binny.nim
@@ -1,12 +1,12 @@
 # Like StringStream but without the Stream and side effects.
 
-func readUint8*(s: string, i: int): uint8 =
+func readUint8*(s: string, i: int): uint8 {.inline.} =
   s[i].uint8
 
-func writeUint8*(s: var string, i: int, v: uint8) =
+func writeUint8*(s: var string, i: int, v: uint8) {.inline.} =
   s[i] = v.char
 
-func addUint8*(s: var string, v: uint8) =
+func addUint8*(s: var string, v: uint8) {.inline.} =
   s.add v.char
 
 when defined(js):
@@ -70,88 +70,88 @@ when defined(js):
     s.add ((v and (0xFF.uint64 shl 48)) shr 48).char
     s.add ((v and (0xFF.uint64 shl 56)) shr 56).char
 else:
-  func readUint16*(s: string, i: int): uint16 =
+  func readUint16*(s: string, i: int): uint16 {.inline.} =
     result = cast[ptr uint16](s[i].unsafeAddr)[]
 
-  func writeUint16*(s: var string, i: int, v: uint16) =
+  func writeUint16*(s: var string, i: int, v: uint16) {.inline.} =
     cast[ptr uint16](s[i].addr)[] = v
 
-  func addUint16*(s: var string, v: uint16) =
+  func addUint16*(s: var string, v: uint16) {.inline.} =
     s.setLen(s.len + sizeof(v))
     cast[ptr uint16](s[s.len - sizeof(v)].addr)[] = v
 
-  func readUint32*(s: string, i: int): uint32 =
+  func readUint32*(s: string, i: int): uint32 {.inline.} =
     result = cast[ptr uint32](s[i].unsafeAddr)[]
 
-  func writeUint32*(s: var string, i: int, v: uint32) =
+  func writeUint32*(s: var string, i: int, v: uint32) {.inline.} =
     cast[ptr uint32](s[i].addr)[] = v
 
-  func addUint32*(s: var string, v: uint32) =
+  func addUint32*(s: var string, v: uint32) {.inline.} =
     s.setLen(s.len + sizeof(v))
     cast[ptr uint32](s[s.len - sizeof(v)].addr)[] = v
 
-  func readUint64*(s: string, i: int): uint64 =
+  func readUint64*(s: string, i: int): uint64 {.inline.} =
     result = cast[ptr uint64](s[i].unsafeAddr)[]
 
-  func writeUint64*(s: var string, i: int, v: uint64) =
+  func writeUint64*(s: var string, i: int, v: uint64) {.inline.} =
     cast[ptr uint64](s[i].addr)[] = v
 
-  func addUint64*(s: var string, v: uint64) =
+  func addUint64*(s: var string, v: uint64) {.inline.} =
     s.setLen(s.len + sizeof(v))
     cast[ptr uint64](s[s.len - sizeof(v)].addr)[] = v
 
-func readInt8*(s: string, i: int): int8 =
+func readInt8*(s: string, i: int): int8 {.inline.} =
   cast[int8](s.readUint8(i))
 
-func writeInt8*(s: var string, i: int, v: int8) =
+func writeInt8*(s: var string, i: int, v: int8) {.inline.} =
   s.writeUint8(i, cast[uint8](v))
 
-func addInt8*(s: var string, v: int8) =
+func addInt8*(s: var string, v: int8) {.inline.} =
   s.addUint8(cast[uint8](v))
 
-func readInt16*(s: string, i: int): int16 =
+func readInt16*(s: string, i: int): int16 {.inline.} =
   cast[int16](s.readUint16(i))
 
-func writeInt16*(s: var string, i: int, v: int16) =
+func writeInt16*(s: var string, i: int, v: int16) {.inline.} =
   s.writeUint16(i, cast[uint16](v))
 
-func addInt16*(s: var string, i: int16) =
+func addInt16*(s: var string, i: int16) {.inline.} =
   s.addUint16(cast[uint16](i))
 
-func readInt32*(s: string, i: int): int32 =
+func readInt32*(s: string, i: int): int32 {.inline.} =
   cast[int32](s.readUint32(i))
 
-func writeInt32*(s: var string, i: int, v: int32) =
+func writeInt32*(s: var string, i: int, v: int32) {.inline.} =
   s.writeUint32(i, cast[uint32](v))
 
-func addInt32*(s: var string, i: int32) =
+func addInt32*(s: var string, i: int32) {.inline.} =
   s.addUint32(cast[uint32](i))
 
-func readInt64*(s: string, i: int): int64 =
+func readInt64*(s: string, i: int): int64 {.inline.} =
   cast[int64](s.readUint64(i))
 
-func writeInt64*(s: var string, i: int, v: int64) =
+func writeInt64*(s: var string, i: int, v: int64) {.inline.} =
   s.writeUint64(i, cast[uint64](v))
 
-func addInt64*(s: var string, i: int64) =
+func addInt64*(s: var string, i: int64) {.inline.} =
   s.addUint64(cast[uint64](i))
 
-func readFloat32*(s: string, i: int): float32 =
+func readFloat32*(s: string, i: int): float32 {.inline.} =
   cast[float32](s.readUint32(i))
 
-func addFloat32*(s: var string, v: float32) =
+func addFloat32*(s: var string, v: float32) {.inline.} =
   s.addUint32(cast[uint32](v))
 
-func readFloat64*(s: string, i: int): float64 =
+func readFloat64*(s: string, i: int): float64 {.inline.} =
   cast[float64](s.readUint64(i))
 
-func addFloat64*(s: var string, v: float64) =
+func addFloat64*(s: var string, v: float64) {.inline.} =
   s.addUint64(cast[uint64](v))
 
-func addStr*(s: var string, v: string) =
+func addStr*(s: var string, v: string) {.inline.} =
   s.add(v)
 
-func readStr*(s: string, i: int, v: int): string =
+func readStr*(s: string, i: int, v: int): string {.inline.} =
   s[i ..< min(s.len, i + v)]
 
 func swap*(v: uint8): uint8 =
