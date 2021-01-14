326b2afa53c92f57f4454bff348e5fbfe2c77724
treeform <starplant@gmail.com>
2021-01-14 09:57:31 -0800

    Switch from func to proc. Some times you just want side effects.

--- src/flatty.nim
+++ src/flatty.nim
@@ -3,107 +3,107 @@
 import flatty/binny, tables, typetraits, flatty/objvar
 
 # Forward declarations.
-func toFlatty[T](s: var string, x: seq[T])
-func toFlatty(s: var string, x: object)
-func toFlatty(s: var string, x: ref object)
-func toFlatty[T: distinct](s: var string, x: T)
-func toFlatty[K, V](s: var string, x: Table[K, V])
-func toFlatty[N, T](s: var string, x: array[N, T])
-func toFlatty[T: tuple](s: var string, x: T)
-func fromFlatty[T](s: string, i: var int, x: var seq[T])
-func fromFlatty(s: string, i: var int, x: var object)
-func fromFlatty(s: string, i: var int, x: var ref object)
-func fromFlatty[T: distinct](s: string, i: var int, x: var T)
-func fromFlatty[K, V](s: string, i: var int, x: var Table[K, V])
-func fromFlatty[N, T](s: string, i: var int, x: var array[N, T])
-func fromFlatty[T: tuple](s: string, i: var int, x: var T)
-func fromFlatty*[T](s: string, x: typedesc[T]): T
+proc toFlatty*[T](s: var string, x: seq[T])
+proc toFlatty*(s: var string, x: object)
+proc toFlatty*(s: var string, x: ref object)
+proc toFlatty*[T: distinct](s: var string, x: T)
+proc toFlatty*[K, V](s: var string, x: Table[K, V])
+proc toFlatty*[N, T](s: var string, x: array[N, T])
+proc toFlatty*[T: tuple](s: var string, x: T)
+proc fromFlatty*[T](s: string, i: var int, x: var seq[T])
+proc fromFlatty*(s: string, i: var int, x: var object)
+proc fromFlatty*(s: string, i: var int, x: var ref object)
+proc fromFlatty*[T: distinct](s: string, i: var int, x: var T)
+proc fromFlatty*[K, V](s: string, i: var int, x: var Table[K, V])
+proc fromFlatty*[N, T](s: string, i: var int, x: var array[N, T])
+proc fromFlatty*[T: tuple](s: string, i: var int, x: var T)
+proc fromFlatty*[T](s: string, x: typedesc[T]): T
 
 # Booleans
-func toFlatty(s: var string, x: bool) =
+proc toFlatty*(s: var string, x: bool) =
   s.addUint8(x.uint8)
 
-func fromFlatty(s: string, i: var int, x: var bool) =
+proc fromFlatty*(s: string, i: var int, x: var bool) =
   x = s.readUint8(i).bool
   i += 1
 
 # Numbers
-func toFlatty(s: var string, x: uint8) = s.addUint8(x)
-func toFlatty(s: var string, x: int8) = s.addInt8(x)
-func toFlatty(s: var string, x: uint16) = s.addUint16(x)
-func toFlatty(s: var string, x: int16) = s.addInt16(x)
-func toFlatty(s: var string, x: uint32) = s.addUint32(x)
-func toFlatty(s: var string, x: int32) = s.addInt32(x)
-func toFlatty(s: var string, x: uint64) = s.addUint64(x)
-func toFlatty(s: var string, x: int64) = s.addInt64(x)
-func toFlatty(s: var string, x: float32) = s.addFloat32(x)
-func toFlatty(s: var string, x: float64) = s.addFloat64(x)
+proc toFlatty*(s: var string, x: uint8) = s.addUint8(x)
+proc toFlatty*(s: var string, x: int8) = s.addInt8(x)
+proc toFlatty*(s: var string, x: uint16) = s.addUint16(x)
+proc toFlatty*(s: var string, x: int16) = s.addInt16(x)
+proc toFlatty*(s: var string, x: uint32) = s.addUint32(x)
+proc toFlatty*(s: var string, x: int32) = s.addInt32(x)
+proc toFlatty*(s: var string, x: uint64) = s.addUint64(x)
+proc toFlatty*(s: var string, x: int64) = s.addInt64(x)
+proc toFlatty*(s: var string, x: float32) = s.addFloat32(x)
+proc toFlatty*(s: var string, x: float64) = s.addFloat64(x)
 
-func fromFlatty(s: string, i: var int, x: var uint8) =
+proc fromFlatty*(s: string, i: var int, x: var uint8) =
   x = s.readUint8(i)
   i += 1
 
-func fromFlatty(s: string, i: var int, x: var int8) =
+proc fromFlatty*(s: string, i: var int, x: var int8) =
   x = s.readInt8(i)
   i += 1
 
-func fromFlatty(s: string, i: var int, x: var uint16) =
+proc fromFlatty*(s: string, i: var int, x: var uint16) =
   x = s.readUint16(i)
   i += 2
 
-func fromFlatty(s: string, i: var int, x: var int16) =
+proc fromFlatty*(s: string, i: var int, x: var int16) =
   x = s.readInt16(i)
   i += 2
 
-func fromFlatty(s: string, i: var int, x: var uint32) =
+proc fromFlatty*(s: string, i: var int, x: var uint32) =
   x = s.readUint32(i)
   i += 4
 
-func fromFlatty(s: string, i: var int, x: var int32) =
+proc fromFlatty*(s: string, i: var int, x: var int32) =
   x = s.readInt32(i)
   i += 4
 
-func fromFlatty(s: string, i: var int, x: var uint64) =
+proc fromFlatty*(s: string, i: var int, x: var uint64) =
   x = s.readUint64(i)
   i += 8
 
-func fromFlatty(s: string, i: var int, x: var int64) =
+proc fromFlatty*(s: string, i: var int, x: var int64) =
   x = s.readInt64(i)
   i += 8
 
-func fromFlatty(s: string, i: var int, x: var int) =
+proc fromFlatty*(s: string, i: var int, x: var int) =
   x = s.readInt64(i).int
   i += 8
 
-func fromFlatty(s: string, i: var int, x: var float32) =
+proc fromFlatty*(s: string, i: var int, x: var float32) =
   x = s.readFloat32(i)
   i += 4
 
-func fromFlatty(s: string, i: var int, x: var float64) =
+proc fromFlatty*(s: string, i: var int, x: var float64) =
   x = s.readFloat64(i)
   i += 8
 
 # Enums
-func toFlatty[T: enum](s: var string, x: T) =
+proc toFlatty*[T: enum](s: var string, x: T) =
   s.addInt64(x.int)
 
-func fromFlatty[T: enum](s: string, i: var int, x: var T) =
+proc fromFlatty*[T: enum](s: string, i: var int, x: var T) =
   x = cast[T](s.readInt64(i))
   i += 8
 
 # Strings
-func toFlatty(s: var string, x: string) =
+proc toFlatty*(s: var string, x: string) =
   s.addInt64(x.len)
   s.add(x)
 
-func fromFlatty(s: string, i: var int, x: var string) =
+proc fromFlatty*(s: string, i: var int, x: var string) =
   let len = s.readInt64(i).int
   i += 8
   x = s[i ..< i + len]
   i += len
 
 # Seq
-func toFlatty[T](s: var string, x: seq[T]) =
+proc toFlatty*[T](s: var string, x: seq[T]) =
   s.addInt64(x.len.int64)
   when not defined(js) and T.supportsCopyMem:
     if x.len == 0:
@@ -116,7 +116,7 @@ func toFlatty[T](s: var string, x: seq[T]) =
     for e in x:
       s.toFlatty(e)
 
-func fromFlatty[T](s: string, i: var int, x: var seq[T]) =
+proc fromFlatty*[T](s: string, i: var int, x: var seq[T]) =
   let len = s.readInt64(i)
   i += 8
   x.setLen(len)
@@ -128,7 +128,7 @@ func fromFlatty[T](s: string, i: var int, x: var seq[T]) =
       s.fromFlatty(i, x[j])
 
 # Objects
-func toFlatty(s: var string, x: object) =
+proc toFlatty*(s: var string, x: object) =
   when x.isObjectVariant:
     s.toFlatty(x.discriminatorField)
     for k, e in x.fieldPairs:
@@ -138,7 +138,7 @@ func toFlatty(s: var string, x: object) =
     for _, e in x.fieldPairs:
       s.toFlatty(e)
 
-func fromFlatty(s: string, i: var int, x: var object) =
+proc fromFlatty*(s: string, i: var int, x: var object) =
   when x.isObjectVariant:
     var discriminator: type(x.discriminatorField)
     s.fromFlatty(i, discriminator)
@@ -150,7 +150,7 @@ func fromFlatty(s: string, i: var int, x: var object) =
     for _, e in x.fieldPairs:
       s.fromFlatty(i, e)
 
-func toFlatty(s: var string, x: ref object) =
+proc toFlatty*(s: var string, x: ref object) =
   let isNil = x == nil
   s.toFlatty(isNil)
   if not isNil:
@@ -163,7 +163,7 @@ func toFlatty(s: var string, x: ref object) =
       for _, e in x[].fieldPairs:
         s.toFlatty(e)
 
-func fromFlatty(s: string, i: var int, x: var ref object) =
+proc fromFlatty*(s: string, i: var int, x: var ref object) =
   var isNil: bool
   s.fromFlatty(i, isNil)
   if not isNil:
@@ -180,20 +180,20 @@ func fromFlatty(s: string, i: var int, x: var ref object) =
         s.fromFlatty(i, e)
 
 # Distinct
-func toFlatty[T: distinct](s: var string, x: T) =
+proc toFlatty*[T: distinct](s: var string, x: T) =
   s.toFlatty(x.distinctBase)
 
-func fromFlatty[T: distinct](s: string, i: var int, x: var T) =
+proc fromFlatty*[T: distinct](s: string, i: var int, x: var T) =
   s.fromFlatty(i, x.distinctBase)
 
 # Tables
-func toFlatty[K, V](s: var string, x: Table[K, V]) =
+proc toFlatty*[K, V](s: var string, x: Table[K, V]) =
   s.addInt64(x.len.int64)
   for k, v in x:
     s.toFlatty(k)
     s.toFlatty(v)
 
-func fromFlatty[K, V](s: string, i: var int, x: var Table[K, V]) =
+proc fromFlatty*[K, V](s: string, i: var int, x: var Table[K, V]) =
   let len = s.readInt64(i)
   i += 8
   for _ in 0 ..< len:
@@ -205,7 +205,7 @@ func fromFlatty[K, V](s: string, i: var int, x: var Table[K, V]) =
     x[k] = v
 
 # Arrays
-func toFlatty[N, T](s: var string, x: array[N, T]) =
+proc toFlatty*[N, T](s: var string, x: array[N, T]) =
   when not defined(js) and T.supportsCopyMem:
     if x.len == 0:
       return
@@ -217,7 +217,7 @@ func toFlatty[N, T](s: var string, x: array[N, T]) =
     for e in x:
       s.toFlatty(e)
 
-func fromFlatty[N, T](s: string, i: var int, x: var array[N, T]) =
+proc fromFlatty*[N, T](s: string, i: var int, x: var array[N, T]) =
   when not defined(js) and T.supportsCopyMem:
     if x.len > 0:
       copyMem(x[0].addr, s[i].unsafeAddr, sizeof(x))
@@ -226,19 +226,19 @@ func fromFlatty[N, T](s: string, i: var int, x: var array[N, T]) =
       s.fromFlatty(i, x[j])
 
 # Tuples
-func toFlatty[T: tuple](s: var string, x: T) =
+proc toFlatty*[T: tuple](s: var string, x: T) =
   for _, e in x.fieldPairs:
     s.toFlatty(e)
 
-func fromFlatty[T: tuple](s: string, i: var int, x: var T) =
+proc fromFlatty*[T: tuple](s: string, i: var int, x: var T) =
   for _, e in x.fieldPairs:
     s.fromFlatty(i, e)
 
-func toFlatty*[T](x: T): string =
+proc toFlatty*[T](x: T): string =
   ## Takes structures and turns them into binary string.
   result.toFlatty(x)
 
-func fromFlatty*[T](s: string, x: typedesc[T]): T =
+proc fromFlatty*[T](s: string, x: typedesc[T]): T =
   ## Takes binary string and turn into structures.
   var i = 0
   s.fromFlatty(i, result)
