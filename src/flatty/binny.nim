8b33069ea0cf596b2f566b61da039021c3bf57d6
Andre von Houck <starplant@gmail.com>
2021-02-11 18:06:47 +0000

    Remove unsued code.

--- src/flatty/binny.nim
+++ src/flatty/binny.nim
@@ -110,53 +110,26 @@ func readStr*(s: seq[uint8], i: int, v: int): string {.inline.} =
 func swap*(v: uint8): uint8 {.inline.} =
   v
 
-when defined(js):
-  func swap*(v: uint16): uint16 =
-    ((v and 0x00FF) shl 8) +
-    ((v and 0xFF00) shr 8)
+func swap*(v: uint16): uint16 {.inline.} =
+  let tmp = cast[array[2, uint8]](v)
+  (tmp[0].uint16 shl 8) or tmp[1].uint16
 
-  func swap*(v: uint32): uint32 =
-    ((v and (0xFF.uint32 shl 0)) shl 24) +
-    ((v and (0xFF.uint32 shl 8)) shl 8) +
-    ((v and (0xFF.uint32 shl 16)) shr 8) +
-    ((v and (0xFF.uint32 shl 24)) shr 24)
+func swap*(v: uint32): uint32 {.inline.} =
+  let tmp = cast[array[2, uint16]](v)
+  (swap(tmp[0]).uint32 shl 16) or swap(tmp[1])
 
-  func swap*(v: int16): int16 {.inline.} =
-    toInt16(toUint16(v).swap())
+func swap*(v: uint64): uint64 {.inline.} =
+  let tmp = cast[array[2, uint32]](v)
+  (swap(tmp[0]).uint64 shl 32) or swap(tmp[1])
 
-  func swap*(v: int32): int32 {.inline.} =
-    cast[int32](cast[uint32](v).swap())
+func swap*(v: int16): int16 {.inline.} =
+  cast[int16](cast[uint16](v).swap())
 
-  func swap*(v: uint64): uint64 =
-    ((v and (0xFF.uint64 shl 0)) shl 56) +
-    ((v and (0xFF.uint64 shl 8)) shl 40) +
-    ((v and (0xFF.uint64 shl 16)) shl 24) +
-    ((v and (0xFF.uint64 shl 24)) shl 8) +
-    ((v and (0xFF.uint64 shl 32)) shr 8) +
-    ((v and (0xFF.uint64 shl 40)) shr 24) +
-    ((v and (0xFF.uint64 shl 48)) shr 40) +
-    ((v and (0xFF.uint64 shl 56)) shr 56)
-else:
-  func swap*(v: uint16): uint16 {.inline.} =
-    let tmp = cast[array[2, uint8]](v)
-    (tmp[0].uint16 shl 8) or tmp[1].uint16
+func swap*(v: int32): int32 {.inline.} =
+  cast[int32](cast[uint32](v).swap())
 
-  func swap*(v: uint32): uint32 {.inline.} =
-    let tmp = cast[array[2, uint16]](v)
-    (swap(tmp[0]).uint32 shl 16) or swap(tmp[1])
-
-  func swap*(v: uint64): uint64 {.inline.} =
-    let tmp = cast[array[2, uint32]](v)
-    (swap(tmp[0]).uint64 shl 32) or swap(tmp[1])
-
-  func swap*(v: int16): int16 {.inline.} =
-    cast[int16](cast[uint16](v).swap())
-
-  func swap*(v: int32): int32 {.inline.} =
-    cast[int32](cast[uint32](v).swap())
-
-  func swap*(v: int64): int64 {.inline.} =
-    cast[int64](cast[uint64](v).swap())
+func swap*(v: int64): int64 {.inline.} =
+  cast[int64](cast[uint64](v).swap())
 
 func maybeSwap*[T](v: T, enable: bool): T =
   if enable:
